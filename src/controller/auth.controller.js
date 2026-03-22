1c843ff44f37bda9a0e57994ad57e704dd107faa
Kanak Yadav <yadavkanak109@gmail.com>
2026-03-22 12:23:36 +0530

    make get new refresh and access token api for secure outh authentication

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,145 +1,169 @@
-const UserModel = require('../models/user.model')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const UserModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-async function UserRegister(req,res){
+async function UserRegister(req, res) {
+  const { username, email, password } = req.body;
 
-    const {username,email,password} = req.body
+  if (!username || !email || !password) {
+    return res.status(401).json({
+      message: "USERNAME , EMAIL ,PASSWORD IS REQUIRED FOR REGISTER !!",
+    });
+  }
 
-    if(!username || !email || !password){
-        return res.status(401).json({
-        message : "USERNAME , EMAIL ,PASSWORD IS REQUIRED FOR REGISTER !!"
-        })
-    }
+  const isUserExits = await UserModel.findOne({
+    $or: [{ email: email }, { username: username }],
+  });
 
-    const isUserExits = await UserModel.findOne({
-         $or : [
-          {email : email},
-          {username :username}
-         ]
-    })
+  if (isUserExits) {
+    return res.status(401).json({
+      message: "User Already Exits",
+    });
+  }
+  const hashPassword = await bcrypt.hash(password, 10);
 
+  const User = await UserModel.create({
+    username,
+    email,
+    password: hashPassword,
+  });
 
-    if(isUserExits){
-        return res.status(401).json({
-            message : "User Already Exits"
-        })
-    }
-    const hashPassword  = await bcrypt.hash(password,10)
+  const token = jwt.sign({ id: User._id }, process.env.JWT_SECRET);
 
-    const User = await UserModel.create({
-        username,
-        email,
-        password : hashPassword
-    })
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
 
-
-    const token  = jwt.sign({id : User._id},process.env.JWT_SECRET)
-
-    res.cookie('token',token,{
-        httpOnly : true,
-        secure : false,
-        sameSite : "strict",
-        maxAge : 7*24*60*60*1000 
-
-    })
-
-
-
-   return res.status(201).json({
-        message : "User Login Sucessfully",
-        User,
-        token
-    })
-
-
-
+  return res.status(201).json({
+    message: "User Login Sucessfully",
+    User,
+    token,
+  });
 }
 
-async function UserLogin(req,res){
-   
-    const {email, password} = req.body
+async function UserLogin(req, res) {
+  const { email, password } = req.body;
 
-    if(!email || !password){
-        return res.status(401).json({
-            message : "EMAIL OR PASSWORD IS REQUIRED FOR LOGIN !!"
-        })
-    }
+  if (!email || !password) {
+    return res.status(401).json({
+      message: "EMAIL OR PASSWORD IS REQUIRED FOR LOGIN !!",
+    });
+  }
 
-    const isUserExits = await UserModel.findOne({
-        email
-    })
+  const isUserExits = await UserModel.findOne({
+    email,
+  });
 
-    if(!isUserExits){
-        return res.status(404).json({
-            message : "User Not Found Please Login first"
-        })
-    }
-    const isPasswordRight = await bcrypt.compare(password,isUserExits.password)
- 
+  if (!isUserExits) {
+    return res.status(404).json({
+      message: "User Not Found Please Login first",
+    });
+  }
+  const isPasswordRight = await bcrypt.compare(password, isUserExits.password);
 
-    if(!isPasswordRight){
-        return res.status(401).json({
-            message : "Invalid Password"
-        })
-    }
+  if (!isPasswordRight) {
+    return res.status(401).json({
+      message: "Invalid Password",
+    });
+  }
 
+  const Accesstoken = jwt.sign(
+    { id: isUserExits._id },
+    process.env.JWT_SECRET,
+    { expiresIn: "15m" },
+  );
 
+  const ReFreshToken = jwt.sign(
+    { id: isUserExits._id },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" },
+  );
 
+  res.cookie("token", ReFreshToken, {
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
 
-    const Accesstoken  = jwt.sign({id : isUserExits._id},process.env.JWT_SECRET,{expiresIn : "15m"})
+  return res.status(201).json({
+    message: "User Login Sucessfully",
+    Accesstoken,
+  });
+}
 
-    const ReFreshToken = jwt.sign({id : isUserExits._id},process.env.JWT_SECRET,{expiresIn : "7d"})
+async function GetInfo(req, res) {
+  const token = req.cookies.token;
+  console.log(token);
 
-     res.cookie('token',ReFreshToken,{
-        httpOnly : true,
-        secure : false,
-        sameSite : "strict",
-        maxAge : 7*24*60*60*1000 
+  if (!token) {
+    return res.status(401).json({
+      message: "Token Not Found",
+    });
+  }
 
-    })
+  try {
+    let decoaded = jwt.verify(token, process.env.JWT_SECRET);
 
+    let user = await UserModel.findById(decoaded.id);
 
+    res.status(201).json({
+      message: "Data Fetch Sucessfully",
+      user,
+    });
+  } catch (err) {
+    return res.status(401).json({
+      message: "Invalid Token",
+    });
+  }
+}
+
+async function GetRefreshToken(req, res) {
+  const RefrestToken = req.cookies.token;
+
+  if (!RefrestToken) {
     return res.status(201).json({
-        message : "User Login Sucessfully",
-        Accesstoken
-    })
+      message: "Access Token Not Found ",
+    });
+  }
 
+  try {
+    const decoaded = jwt.verify(RefrestToken, process.env.JWT_SECRET);
 
+    const Accesstoken = jwt.sign({ id: decoaded.id }, process.env.JWT_SECRET);
+
+    const RefreshToken = jwt.sign(
+      { id: decoaded.id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7D",
+      },
+    );
+
+    res.cookie("token", RefreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({
+      message: "Access Token Gernate Sucessfully",
+      Accesstoken,
+    });
+  } catch (err) {
+    return res.status(401).json({
+      message: "Invalid Token",
+    });
+  }
 }
 
-
-async function GetInfo(req,res){
-    const token = req.cookies.token
- console.log(token);
- 
-    if(!token){
-        return res.status(401).json({
-            message : "Token Not Found"
-        })
-    }
-
-    try{
-  
-         let decoaded   = jwt.verify(token,process.env.JWT_SECRET)
-          
-         let user = await UserModel.findById(decoaded.id)
-
-         res.status(201).json({
-            message : "Data Fetch Sucessfully",
-            user
-         })
-
-    }
-    catch(err){
-       return res.status(401).json({
-        message : "Invalid Token"
-       })
-    }
-}
-
-module.exports={
-    UserRegister,
-    UserLogin,
-    GetInfo
-}
+module.exports = {
+  UserRegister,
+  UserLogin,
+  GetInfo,
+  GetRefreshToken,
+};
--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -6,5 +6,6 @@ const router = express.Router()
 router.post('/register',UserAuthController.UserRegister)
 router.post('/login',UserAuthController.UserLogin)
 router.get('/get-info',UserAuthController.GetInfo)
+router.get('/get-refresh-token',UserAuthController.GetRefreshToken)
 
 module.exports = router
